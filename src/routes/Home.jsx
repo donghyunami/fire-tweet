dc2483963d5d417af65199da4c6cf0c0fcdd610a
Donghyun Lee <skyblue5030@naver.com>
2022-07-28 18:44:41 +0900

    폼과 데이터베이스 Setup

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
-  return <span>Home</span>;
+  const [nweet, setNweet] = useState('');
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(nweet);
+  };
+
+  const onChange = e => {
+    const {
+      target: { value },
+    } = e;
+
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={nweet}
+          onChange={onChange}
+          placeholder="무슨 생각을 하고 있나요?"
+          maxLength={120}
+        />
+        <button type="submit">Nweet</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,4 @@
 export { default as Home } from './Home';
-export { default as Auth } from './Auth';
+export { default as Auth } from './AuthForm';
 export { default as Profile } from './Profile';
-export { default as EditProfile } from './Auth';
+export { default as EditProfile } from './AuthForm';
