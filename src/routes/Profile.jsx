41b06a997949fd1b8975d148bdac59ccbdb8685b
Dhyunlee <skyblue5030@naver.com>
2022-11-03 12:09:31 +0900

    Get My Own Nweets

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,14 +1,33 @@
-import { authService } from 'fbase';
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { authService, dbService } from "fbase";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 
-const Profile = () => {
+const Profile = ({userObj}) => {
   const history = useHistory();
 
   const onLogOutClick = () => {
     authService.signOut();
-    history.push('/');
+    history.push("/");
   };
+
+  const getMyNweets = async () => {
+    const queryNweets = query(
+      collection(dbService, "nweets"),
+      where("creatorId", "==", userObj.uid),
+      orderBy("createdAt","desc")
+    );
+    const nweets = await getDocs(queryNweets);
+    nweets.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+    });
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
